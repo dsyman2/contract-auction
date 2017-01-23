ff16d5a8d9ced31848ab7884662961bb7c9dc82d
Umar Akhtar <umarakhtar95@gmail.com>
2017-01-23 22:14:01 +0000

    multiple changes, created 'createAuction' end point which will harbour auction table entry stuff, but more importantly created a post request to this end point in angular which was the biggest pain ever but still working at least so can't complain

--- public/javascripts/app/createAuction.component.ts
+++ public/javascripts/app/createAuction.component.ts
@@ -5,7 +5,12 @@
 import {Component} from 'angular2/core';
 import {CORE_DIRECTIVES, NgClass, FORM_DIRECTIVES, Control, ControlGroup, FormBuilder, Validators} from 'angular2/common';
 import {Inject} from "angular2/src/core/di/decorators";
-
+import {Http, Headers, RequestOptions, RequestMethod, Request, Response} from 'angular2/http';
+//import {RequestOptions} from "angular2/src/http/base_request_options";
+//import {RequestMethod} from "angular2/src/http/enums";
+//import {Request} from "angular2/src/http/static_request";
+//import {Response} from "angular2/src/http/static_response";
+import 'rxjs/Rx';
 
 class FormInputs{
     auctionName: string;
@@ -18,14 +23,18 @@ class FormInputs{
 @Component({
     selector: 'createAuction-app',
     templateUrl: '/templates/createAuction.html',
-    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES]
+    directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],
 })
+
 
 export class CreateAuctionComponent {
     CreateGroup: ControlGroup;
     formInputs: FormInputs;
+    requestOptions: RequestOptions;
+    headers: Headers;
+   // http: Http;
 
-    constructor(@Inject(FormBuilder) fb: FormBuilder){
+    constructor(@Inject(FormBuilder) fb: FormBuilder, private http: Http){
         this.formInputs = new FormInputs();
         this.CreateGroup = fb.group({
             'auctionName'   : new Control(this.formInputs.auctionName, Validators.required),
@@ -38,6 +47,42 @@ export class CreateAuctionComponent {
 
     addNewGroup(formInputs : FormInputs) {
         this.formInputs = new FormInputs();
+        var data = {
+            auctionName:    formInputs.auctionName,
+            auctionDesc:    formInputs.auctionDesc,
+            length:         formInputs.length,
+            protocol:       formInputs.protocol,
+            creatorID:      formInputs.creatorID
+        };
 
+        this.addAuctionPostRequest("/createAuction", data);
+    }
+
+    addAuctionPostRequest(url, data) {
+        console.log(data.auctionName);
+       /* let headers = new Headers({
+            'Content-Type': 'application/json'
+        });*/
+
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+
+
+       /* this.requestOptions = new RequestOptions({
+            method: RequestMethod.Post,
+            url: url,
+            headers: this.headers,
+            body: JSON.stringify(data)
+        });
+
+        return this.http.request(new Request(this.requestOptions))
+            .map((res: Response) => {
+                if(res) {
+                    return [{status: res.status, json: res.json() }]
+                }
+            });*/
+
+        this.http.post(url, JSON.stringify(data), {headers: this.headers})
+            .map(res => console.log(res.json())).subscribe();
     }
 }
