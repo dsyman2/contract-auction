2e482abcfe66dd008e0bcd919d1abb3c6cade45f
Umar Akhtar <umarakhtar95@gmail.com>
2017-03-05 21:20:49 +0000

    now dislpaying rhe dodgy user and the reason why they are

--- public/javascripts/app/config/configer.js
+++ public/javascripts/app/config/configer.js
@@ -1,16 +1,6 @@
-System.register([], function(exports_1, context_1) {
-    /**
-     * Created by Umar on 20/02/2017.
-     */
-    'use strict';
-    var __moduleName = context_1 && context_1.id;
-    var socket_src;
-    return {
-        setters:[],
-        execute: function() {
-            //export var socket_src: string ='http://ec2-52-56-141-53.eu-west-2.compute.amazonaws.com:8000';
-            exports_1("socket_src", socket_src = 'http://localhost:8000');
-        }
-    }
-});
-//# sourceMappingURL=configer.js.map
+/**
+ * Created by Umar on 20/02/2017.
+ */
+'use strict';
+exports.socket_src = 'http://ec2-52-56-141-53.eu-west-2.compute.amazonaws.com:8000';
+//export var socket_src: string = 'http://localhost:8000' 
